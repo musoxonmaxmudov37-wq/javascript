84a5661decb760c3013f5afa200a7ae992f7ca77
musoxonmaxmudov37-wq <musoxonmaxmudov37@gmail.com>
2026-03-10 16:04:36 +0500

    For loop h

---
+++ homewrok/script.js
@@ -0,0 +1,64 @@
+// 1
+
+// for (let i = 1; i <= 20; i ++) {
+//     if (i % 2 === 0) {
+//         continue;
+//     }
+//     console.log("Toq: " + i);
+// }
+
+
+// 2
+
+// let sum = 0, i = 1;
+// while(i < 10) {
+//     sum += i;
+//     i++; 
+// }
+// console.log(sum); 
+
+
+// 3
+
+// let count = 10;
+// while(count >= 1) {
+//     console.log("Teskari: " + count);
+//     count --;
+// }
+
+// let i = 1;
+// do{
+//     console.log( i);
+//     i++;
+// } while (i >= 10);
+
+
+// 4 
+
+// for (i = 1; i <= 50; i++) {
+//     if (i % 7 === 0) {
+//         break;
+//     }
+
+//     console.log(i)
+// }
+
+
+// 5 
+
+// for(i = 1; i <= 30; i++) {
+//     if (i % 3==0 && i % 5 == 0){
+//         console.log(i);
+//     }
+// }
+
+// 6
+
+// for (i = 1; i<= 100; i++) {
+//     if (i % 9 == 0){
+// if (i === 50);
+//         break;
+//     }
+//     console.log(i);
+// }
+
